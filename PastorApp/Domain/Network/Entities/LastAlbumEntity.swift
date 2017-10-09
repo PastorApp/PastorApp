cc7441b45115178f7266401574efea17da1606af
Alan Casas <alancasas@MacBook-Pro-de-Alan.local>
2017-10-09 19:22:39 +0200

    Domain Framework added

---
+++ PastorApp/Domain/Network/Entities/LastAlbumEntity.swift
@@ -0,0 +1,30 @@
+//
+//  LastAlbumEntity.swift
+//  Domain
+//
+//  Created by Alan Casas on 9/10/17.
+//  Copyright © 2017 Alan Casas. All rights reserved.
+//
+
+import Foundation
+
+public class LastAlbumEntity{
+    
+    public let title: String
+    
+    public init (title: String){
+        self.title = title
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
